0fe9f76fc210b03cdea4867a4069be40091b0567
arafah-dhrubo <samiul15-13051@diu.edu.bd>
2023-02-15 17:38:12 +0600

    updated example header row

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 from flask import Flask, render_template, jsonify, request
 from sqlalchemy import create_engine, text
@@ -8,8 +9,8 @@ engine = create_engine('postgresql+psycopg2://postgres:1@localhost:5432/stockdb'
 connection = engine.connect()
 
 
-@app.route("/")
-def index():
+@app.route("/backup")
+def backup():
     query_for_header = f"SELECT column_name FROM information_schema.columns WHERE table_name = 'company'"
     fields = connection.execute(text(query_for_header))
 
@@ -26,8 +27,215 @@ def index():
 
     list = connection.execute(text(query_for_all)).fetchall()
     data = [{headers[i]: tup[i] for i in range(len(headers))} for tup in list]
-
+    print(fields)
     return render_template("example-grid-menu.html", fields=columns, list=data)
+
+
+@app.route("/")
+def index():
+    response = requests.get('http://localhost:5501/shared/loadscreener/')
+
+    if response.status_code == 200:  # Check if the request was successful
+        json_data = response.json()
+        bbo_list = json_data['bbo_list']
+        ltp_list = json_data['ltp_list']
+        symbol_info_list = json_data['symbol_info_list']
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+
+    columns = [
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "id",
+            "id": "id",
+            "name": "id",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "symbol",
+            "id": "symbol",
+            "name": "symbol",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "company_name",
+            "id": "company_name",
+            "name": "Name",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "sector",
+            "id": "sector",
+            "name": "sector",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },{
+            "editor": "Slick.Editors.Text",
+            "field": "ltp",
+            "id": "ltp",
+            "name": "ltp",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },{
+            "editor": "Slick.Editors.Text",
+            "field": "ycp",
+            "id": "ycp",
+            "name": "ycp",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "total_qty",
+            "id": "total_qty",
+            "name": "Volume",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },{
+            "editor": "Slick.Editors.Text",
+            "field": "ltp_change",
+            "id": "ltp_change",
+            "name": "Chg",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },{
+            "editor": "Slick.Editors.Text",
+            "field": "ltp_changeper",
+            "id": "ltp_changeper",
+            "name": "% Chg",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "bid_price",
+            "id": "bid_price",
+            "name": "bid_price",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "bid_qty",
+            "id": "bid_qty",
+            "name": "bid_qty",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "ask_price",
+            "id": "ask_price",
+            "name": "ask_price",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "ask_qty",
+            "id": "ask_qty",
+            "name": "ask_qty",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "total_trades",
+            "id": "total_trades",
+            "name": "Trades",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "total_value",
+            "id": "total_value",
+            "name": "total_value",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "open",
+            "id": "open",
+            "name": "open",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "high",
+            "id": "high",
+            "name": "high",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+        {
+            "editor": "Slick.Editors.Text",
+            "field": "low",
+            "id": "low",
+            "name": "low",
+            "resizable": "true",
+            "selectable": "true",
+            "sortable": "false",
+            "width": 160
+        },
+    ]
+
+    symbol_dict = {item['symbol']: item for item in bbo_list}
+    for item in ltp_list:
+        symbol_dict.get(item['symbol'], {}).update(item, inplace=True)
+
+    combined_list = list(symbol_dict.values())
+
+    combined_dict = {item['symbol']: item for item in combined_list}
+    for item in symbol_info_list:
+        if item['symbol'] in combined_dict:
+            combined_dict[item['symbol']].update(item, inplace=True)
+
+    combined_list = list(combined_dict.values())
+
+    return render_template("example-grid-menu.html", fields=columns, list=combined_list)
 
 
 @app.route("/test")
